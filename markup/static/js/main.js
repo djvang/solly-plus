be78d7345f3167b2a96f021bad297bc830828b41
djvang <djvang92@gmail.com>
2019-02-18 12:37:57 +0200

    fixes

--- markup/static/js/main.js
+++ markup/static/js/main.js
@@ -226,90 +226,101 @@ function swiperInit() {
 
 function priceInit() {
 
-    let price = document.getElementById('price');
+    let roots = document.querySelectorAll('[data-nouislider]');
 
-    if(!price) return;
 
-    let priceFrom = document.getElementById('price-from');
-    let priceTo = document.getElementById('price-to');
-    let inputs = [priceFrom, priceTo];
-    
-    noUiSlider.create(price, {
-        start: [0, 10000],
-        step: 1,
-        connect: false,
-        range: {
-            'min': [0],
-            'max': 10000
-        }
-    });
-    
-    price.noUiSlider.on('update', function (values, handle) {
-        inputs[handle].value = parseFloat(values[handle]) + ' грн';
-    });
-    
-    
-    inputs.forEach(function (input, handle) {
-    
-        input.addEventListener('change', function () {
-            price.noUiSlider.setHandle(handle, this.value.replace(' грн', ''));
-        });
-    
-        input.addEventListener('keydown', function (e) {
-    
-            let values = price.noUiSlider.get();
-            let value = Number(values[handle]).replace(' грн', '');
-    
-            // [[handle0_down, handle0_up], [handle1_down, handle1_up]]
-            let steps = price.noUiSlider.steps();
-    
-            // [down, up]
-            let step = steps[handle];
-    
-            let position;
-    
-            // 13 is enter,
-            // 38 is key up,
-            // 40 is key down.
-            switch (e.which) {
-    
-                case 13:
-                price.noUiSlider.setHandle(handle, this.value.replace(' грн', ''));
-                    break;
-    
-                case 38:
-    
-                    // Get step to go increase slider value (up)
-                    position = step[1];
-    
-                    // false = no step is set
-                    if (position === false) {
-                        position = 1;
-                    }
-    
-                    // null = edge of slider
-                    if (position !== null) {
-                        price.noUiSlider.setHandle(handle, value + position);
-                    }
-    
-                    break;
-    
-                case 40:
-    
-                    position = step[0];
-    
-                    if (position === false) {
-                        position = 1;
-                    }
-    
-                    if (position !== null) {
-                        price.noUiSlider.setHandle(handle, value - position);
-                    }
-    
-                    break;
+    roots.forEach(root => {
+        nouislider(root);
+    })
+
+
+    function nouislider(root) {
+        let price = root.querySelector('[data-nouislider-range]');
+
+        if(!price) return;
+
+        let priceFrom = root.querySelector('[data-nouislider-from]');
+        let priceTo = root.querySelector('[data-nouislider-to]');
+        let inputs = [priceFrom, priceTo];
+        
+        noUiSlider.create(price, {
+            start: [0, 10000],
+            step: 1,
+            connect: false,
+            range: {
+                'min': [0],
+                'max': 10000
             }
         });
-    });
+        
+        price.noUiSlider.on('update', function (values, handle) {
+            inputs[handle].value = parseFloat(values[handle]) + ' грн';
+        });
+        
+        
+        inputs.forEach(function (input, handle) {
+        
+            input.addEventListener('change', function () {
+                price.noUiSlider.setHandle(handle, this.value.replace(' грн', ''));
+            });
+        
+            input.addEventListener('keydown', function (e) {
+        
+                let values = price.noUiSlider.get();
+                let value = Number(values[handle]).replace(' грн', '');
+        
+                // [[handle0_down, handle0_up], [handle1_down, handle1_up]]
+                let steps = price.noUiSlider.steps();
+        
+                // [down, up]
+                let step = steps[handle];
+        
+                let position;
+        
+                // 13 is enter,
+                // 38 is key up,
+                // 40 is key down.
+                switch (e.which) {
+        
+                    case 13:
+                    price.noUiSlider.setHandle(handle, this.value.replace(' грн', ''));
+                        break;
+        
+                    case 38:
+        
+                        // Get step to go increase slider value (up)
+                        position = step[1];
+        
+                        // false = no step is set
+                        if (position === false) {
+                            position = 1;
+                        }
+        
+                        // null = edge of slider
+                        if (position !== null) {
+                            price.noUiSlider.setHandle(handle, value + position);
+                        }
+        
+                        break;
+        
+                    case 40:
+        
+                        position = step[0];
+        
+                        if (position === false) {
+                            position = 1;
+                        }
+        
+                        if (position !== null) {
+                            price.noUiSlider.setHandle(handle, value - position);
+                        }
+        
+                        break;
+                }
+            });
+        });
+    }
+    
 }
 
 function drilldownInit() {
